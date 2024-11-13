b38a3fbca778c913110d2781d7c21c36746d8d94
Clecio Antao <clecio_antao@carrefour.com>
2024-11-13 15:52:03 -0300

    Adjusts in workflow - ajustes variaveis

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,3 +49,23 @@ variable "storage_account_name" {
   type        = string
   default     = "humanitecstorage"
 }
+
+variable "TF_VAR_client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "TF_VAR_client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "TF_VAR_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "TF_VAR_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
