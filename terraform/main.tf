97ea1c158b9690a3b8d54d926f4110fe405e5278
Clecio Antao <clecio_antao@carrefour.com>
2024-11-13 16:05:27 -0300

    Alteração no tipo de redundância do armazenamento

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,13 +37,13 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 }
 
 resource "azurerm_storage_container" "container" {
   name                  = "test-container"
   storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+  container_access_type = "blob"
 }
 
 output "storage_account_name" {
